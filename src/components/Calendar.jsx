6095b0fe99673db70734aded2c01fb0311fe7ace
caro270903 <carolina.vargasv26@gmail.com>
2025-08-26 21:11:04 -0500

    cambio de calendario

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,20 +1,24 @@
 import React, { useState } from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
+import './CalendarCustom.css'; // Importa los estilos personalizados
 
 function CalendarComponent() {
   const [date, setDate] = useState(new Date());
 
   return (
-    <div>
+    <div className="calendar-container">
       <Calendar
         onChange={setDate}
         value={date}
         view="month"
         minDetail="decade"
         maxDetail="month"
+        showNeighboringMonth={false}
+        locale="es-ES"
+        next2Label={null}
+        prev2Label={null}
       />
-      <p>Fecha seleccionada: {date.toLocaleDateString()}</p>
     </div>
   );
 }
