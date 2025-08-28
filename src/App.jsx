9291de62ca08b09920b1102337c492ed352b8f9a
Juanita003 <juanacosato@gmail.com>
2025-08-27 22:39:29 -0500

    Corrección splash

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,38 @@
-import { BrowserRouter, Routes, Route } from "react-router";
+import { useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router";
 import DeviceFrame from "./components/DeviceFrame.jsx";
 import Home from "./pages/Home.jsx";
 import Search from "./pages/Search.jsx";
 import Profile from "./pages/Profile.jsx";
 import AddExpenses from "./pages/AddExpenses.jsx";
 import AddIncome from "./pages/AddIncome.jsx";
+import Splash from "./pages/Splash.jsx"; // ajusta la ruta si tu archivo está en otro lugar
 
 export default function App() {
+  const [ready, setReady] = useState(false);
+
+  useEffect(() => {
+    const t = setTimeout(() => setReady(true), 1500);
+    return () => clearTimeout(t);
+  }, []);
+
   return (
-    <DeviceFrame>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/buscar" element={<Search />} />
-          <Route path="/yo" element={<Profile />} />
-          <Route path="/add/expense" element={<AddExpenses />} />
-          <Route path="/add/income" element={<AddIncome />} />
-        </Routes>
-      </BrowserRouter>
-    </DeviceFrame>
+    <BrowserRouter>
+      <DeviceFrame>
+        {!ready ? (
+          <Splash />
+        ) : (
+          <Routes>
+            {/* Al terminar el splash, "/" redirige a /home */}
+            <Route path="/" element={<Navigate to="/home" replace />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/buscar" element={<Search />} />
+            <Route path="/yo" element={<Profile />} />
+            <Route path="/add/expense" element={<AddExpenses />} />
+            <Route path="/add/income" element={<AddIncome />} />
+          </Routes>
+        )}
+      </DeviceFrame>
+    </BrowserRouter>
   );
 }
--- src/pages/Splash.jsx
+++ src/pages/Splash.jsx
@@ -1,21 +1,13 @@
-import { useEffect } from 'react'
-import { useNavigate } from 'react-router'
-import s from './Splash.module.scss'
-import logoLarge from '../assets/logo-large.png'
-import appIcon from '../assets/app-icon.png'
+import s from "./Splash.module.scss";
+import logoLarge from "../assets/logo-large.png";
+import appIcon from "../assets/app-icon.png";
 
 export default function Splash() {
-  const navigate = useNavigate()
-  useEffect(() => {
-    const t = setTimeout(() => navigate('/home', { replace: true }), 1500)
-    return () => clearTimeout(t)
-  }, [navigate])
-
   return (
     <div className={s.splash}>
       <img src={logoLarge} alt="LukApp" className={s.logoLarge} />
       <img src={appIcon} alt="App icon" className={s.appIcon} />
       <div className={s.fade} />
     </div>
-  )
+  );
 }
